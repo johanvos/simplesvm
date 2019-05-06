fff06d24296cff3b92886766691ee26f28354744
Johan Vos <johan@lodgon.com>
2019-05-06 12:09:40 +0200

    Print Java and JavaFX version

--- src/java/HelloFX.java
+++ src/java/HelloFX.java
@@ -12,15 +12,16 @@ public class HelloFX extends Application {
     @Override
     public void start(Stage stage) {
         System.err.println("[JVDBG] APPLICATION HELLOFX start");
+        String jfxversion = System.getProperty("javafx.version");
         String version = System.getProperty("java.version");
-        Label l = new Label ("Hello, JavaFX 11, running on "+version);
+        Label l = new Label ("Hello, JavaFX "+jfxversion+", running on Java "+version);
         Scene scene = new Scene (l, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        System.setProperty("os.target", "ios");
+        // System.setProperty("os.target", "ios");
         System.setProperty("javafx.verbose", "true");
         System.setProperty("prism.verbose", "true");
         launch();
