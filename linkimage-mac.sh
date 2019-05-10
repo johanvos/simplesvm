fb9661b11ac4508422f9e972b9d696d404d9428b
Johan Vos <johan.vos@gluonhq.com>
2019-05-10 18:50:19 +0200

    Add script to build with JNI platform on mac

--- linkimage-mac.sh
+++ linkimage-mac.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SVMBUILD=/Users/johan/graal/github/fork/graal/substratevm/svmbuild/native-image-root-11/
 SDK=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk
+JFXLIB=/Users/johan/open-jfx/github/forks/openjdk-jfx/build/sdk/lib
 PWD=`pwd`
 echo "this should contain hellofxsvm.o: $1"
 mkdir -p $PWD/build/mac
@@ -8,6 +9,7 @@ echo "compiling\n=========\n"
 gcc -c -o $PWD/build/mac/launcher.o $PWD/src/native/mac/launcher.c
 gcc -c -o $PWD/build/mac/AppDelegate.o $PWD/src/native/mac/AppDelegate.m
 echo "linking\n=======\n"
-gcc -isysroot $SDK -fobjc-arc -o build/mac/hellofx  build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -lffi -Wl,-framework,CoreFoundation -Wl,-framework,Appkit -lpthread -lz -ldl -lstrictmath -llibchelper
+echo "-isysroot $SDK -fobjc-arc -o build/mac/hellofx  build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$JFXLIB -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -lffi -Wl,-framework,CoreFoundation -Wl,-framework,Appkit -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2"
+#gcc -isysroot $SDK -fobjc-arc -o build/mac/hellofx  build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$JFXLIB -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -lffi -Wl,-framework,CoreFoundation -Wl,-framework,Appkit -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2
 echo "running\n=======\n"
 ./build/mac/hellofx
