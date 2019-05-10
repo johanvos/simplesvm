a90fc53734c34627724f9213dd628c8442b2417f
Johan Vos <johan.vos@gluonhq.com>
2019-05-10 19:00:27 +0200

    add static libs and link script

--- linkimage-mac-jni.sh
+++ linkimage-mac-jni.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 SVMBUILD=/Users/johan/graal/github/fork/graal/substratevm/svmbuild/native-image-root-11/
 SDK=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk
-JFXLIB=/Users/johan/open-jfx/github/forks/openjdk-jfx/build/sdk/lib
 PWD=`pwd`
 echo "this should contain helloworldsvmjni.o: $1"
 mkdir -p $PWD/build/mac
@@ -9,7 +8,7 @@ echo "compiling\n=========\n"
 gcc -c -o $PWD/build/mac/launcher.o $PWD/src/native/mac/launcher.c
 gcc -c -o $PWD/build/mac/AppDelegate.o $PWD/src/native/mac/AppDelegate.m
 echo "linking\n=======\n"
-echo "gcc -isysroot $SDK -fobjc-arc -o build/mac/hellofxsvm -Wl,-exported_symbols_list,release.symbols build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$JFXLIB -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -L/Users/johan/openjdk/jdk/build/macosx-x86_64-server-release/jdk/lib -lffi -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2 -lglass -ljavafx_font -ljava -lnio -lzip -lnet /Users/johan/graal/github/fork/graal//substratevm/mxbuild/darwin-amd64/SVM_GRAALVM_SUPPORT/builder/clibraries/darwin-amd64/libjvm.a -Wl,-framework,Foundation -Wl,-framework,AppKit -Wl,-framework,ApplicationServices -Wl,-framework,OpenGL -Wl,-framework,QuartzCore -Wl,-framework,Security"
-gcc -ObjC -isysroot $SDK -iframework$SDK/System/Library/Frameworks -arch x86_64 -o build/mac/hellofxsvm -Wl,-exported_symbols_list,release.symbols build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$JFXLIB -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -L/Users/johan/openjdk/jdk/build/macosx-x86_64-server-release/jdk/lib -lffi -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2 -lglass -ljavafx_font -ljava -lnio -lzip -lnet /Users/johan/graal/github/fork/graal//substratevm/mxbuild/darwin-amd64/SVM_GRAALVM_SUPPORT/builder/clibraries/darwin-amd64/libjvm.a -Wl,-framework,Foundation -Wl,-framework,AppKit -Wl,-framework,ApplicationServices -Wl,-framework,OpenGL -Wl,-framework,QuartzCore -Wl,-framework,Security -lobjc
+echo "gcc -ObjC -isysroot $SDK -iframework$SDK/System/Library/Frameworks -arch x86_64 -o build/mac/hellofxsvm -Wl,-exported_symbols_list,release.symbols build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -L$PWD/staticlibs -lffi -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2 -lglass -ljavafx_font -ljava -lnio -lzip -lnet /Users/johan/graal/github/fork/graal//substratevm/mxbuild/darwin-amd64/SVM_GRAALVM_SUPPORT/builder/clibraries/darwin-amd64/libjvm.a -Wl,-framework,Foundation -Wl,-framework,AppKit -Wl,-framework,ApplicationServices -Wl,-framework,OpenGL -Wl,-framework,QuartzCore -Wl,-framework,Security -lobjc"
+gcc -ObjC -isysroot $SDK -iframework$SDK/System/Library/Frameworks -arch x86_64 -o build/mac/hellofxsvm -Wl,-exported_symbols_list,release.symbols build/mac/AppDelegate.o build/mac/launcher.o $1/hellofxsvm.o -L$SVMBUILD/lib/svm/clibraries/darwin-amd64 -L$PWD/staticlibs -lffi -lpthread -lz -ldl -lstrictmath -llibchelper -lprism_es2 -lglass -ljavafx_font -ljava -lnio -lzip -lnet -ljvm -Wl,-framework,Foundation -Wl,-framework,AppKit -Wl,-framework,ApplicationServices -Wl,-framework,OpenGL -Wl,-framework,QuartzCore -Wl,-framework,Security -lobjc
 echo "running\n=======\n"
 #./build/mac/hellofx
