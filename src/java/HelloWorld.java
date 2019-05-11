58f166dc0c8206ede3519a713ed89ecdb2ca58fa
Johan Vos <johan.vos@gluonhq.com>
2019-05-11 12:34:35 +0200

    add more classes for reflection add nanotime symbol add new OpenJFX libs

--- src/java/HelloWorld.java
+++ src/java/HelloWorld.java
@@ -1,13 +1,31 @@
+import java.util.logging.*;
+import java.io.*;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.err.println("hello, world");
+    public static void main(String[] args) throws Exception  {
+        System.err.println("Hello, world, in main");
+File f = new File("/Users/johan/ftt");
+String[] cc = f.list();
+for (String c: cc) {
+System.err.println("FILE -> "+c);
+}
+System.load("/Users/johan/dooh");
+System.err.println("SCL = "+ClassLoader.getSystemClassLoader());
+System.err.println("PCL = "+ClassLoader.getPlatformClassLoader());
+Thread.dumpStack();
         Thread t = new Thread() {
             @Override public void run() {
                 System.err.println("Running in a new thread");
             }
         };
         t.start();
+ Logger l = Logger.getLogger(HelloWorld.class.getName());
+               System.err.println("Hello, world");
+                System.Logger syslog = System.getLogger("foo");
+                syslog.log(System.Logger.Level.INFO, "Hello, syslog");
+       l.info("Hello, normal log");
+
         try {
             System.err.println("Started new thread, main thread will sleep 10 seconds now");
             Thread.sleep(10000);
