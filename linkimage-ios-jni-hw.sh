3ec995435f5bc0ee43f3dcb52f242ca1816d6276
Johan Vos <johan.vos@gluonhq.com>
2019-05-16 12:42:57 +0200

    add more ios changes

---
+++ linkimage-ios-jni-hw.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+SVMBUILD=/Users/johan/graal/github/fork/graal/substratevm/svmbuild/native-image-root-11/
+SDK=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator12.2.sdk
+JFXLIB=/Users/johan/open-jfx/github/forks/openjdk-jfx/build/ios-sdk/lib
+PWD=`pwd`
+echo "this should contain hellofxsvm.o: $1"
+mkdir -p $PWD/build/ios
+echo "compiling\n=========\n"
+gcc -c -isysroot $SDK -o $PWD/build/ios/main.o $PWD/src/native/ios/main.m
+gcc -c -isysroot $SDK -o $PWD/build/ios/AppDelegate.o $PWD/src/native/ios/AppDelegate.m
+echo "linking\n=======\n"
+mkdir -p build/ios/hellofx.app
+clang++ -w -o build/ios/hellofx.app/hellofx \
+-Wl,-no_implicit_dylibs \
+-Wl,-dead_strip \
+-fPIC \
+-isysroot $SDK -arch x86_64 -mios-version-min=8.0 \
+-Wl,-exported_symbols_list,$PWD/src/native/ios/release.symbols \
+-Wl,-all_load,$JFXLIB/libprism_es2.a \
+-Wl,-all_load,$JFXLIB/libglass.a \
+-Wl,-all_load,$JFXLIB/libjavafx_font.a \
+build/ios/AppDelegate.o build/ios/main.o $1/helloworldsvmjni.o \
+-L$SVMBUILD/lib/svm/clibraries/darwin-amd64 \
+-L$PWD/staticlibs/ios \
+-lffi \
+-lpthread -lz  -lstrictmath -llibchelper \
+-ljava -lnio -lzip -lnet -ljvm \
+-Wl,-framework,Foundation \
+-Wl,-framework,CoreGraphics \
+-Wl,-framework,OpenGLES \
+-Wl,-framework,MobileCoreServices \
+-Wl,-framework,CoreText \
+-Wl,-framework,ImageIO \
+-Wl,-framework,UserNotifications \
+-Wl,-framework,CoreBluetooth \
+-Wl,-framework,CoreLocation \
+-Wl,-framework,CoreMedia \
+-Wl,-framework,AVFoundation \
+-Wl,-framework,Accelerate \
+-Wl,-framework,CoreVideo \
+-Wl,-framework,QuartzCore \
+-Wl,-framework,UIKit
+echo "preparing app\n=======\n"
+cp src/native/ios/Info.plist build/ios/hellofx.app
+echo "launching app\n=======\n"
+$PWD/src/native/ios/simlauncher  --udid=C3E4154D-00E8-4656-A284-FE9073ECAA10 --app-path=$PWD/build/ios/hellofx.app
---
+++ src/native/ios/mythread.m
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+void JVM_StartThread() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_ResumeThread() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_Sleep() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_HoldsLock() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_IsThreadAlive() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_Interrupt() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_IsInterrupted() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_SetNativeThreadName() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_GetAllThreads() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_SetThreadPriority() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_CurrentThread() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_CountStackFrames() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_StopThread() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_SuspendThread() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_Yield() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_interrupt() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void JVM_DumpThreads() {
+fprintf(stderr, "THREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_resume0() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_stop0() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_getThreads() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_countStackFrames() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_dumpThreads() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
+
+void Java_java_lang_Thread_suspend0() {
+fprintf(stderr, "JAVATHREADIMPLMISSING\n");
+}
