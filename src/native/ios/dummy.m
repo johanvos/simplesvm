260d790cb1c74276e819f355605331aa2b049520
Johan Vos <johan.vos@gluonhq.com>
2019-05-25 11:05:00 +0200

    add ios specific symbol file and dummy definitions

---
+++ src/native/ios/dummy.m
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void StrictMath_log() {
+    fprintf(stderr, "STRICTMATHLOG NYI\n");
+}
+void fstat$INODE64() {
+    fprintf(stderr, "fstatINODE64 NYI\n");
+}
+void stat$INODE64() {
+    fprintf(stderr, "statINODE64 NYI\n");
+}
+void StrictMath_pow() {
+    fprintf(stderr, "STRICTMATHPOW NYI\n");
+}
